544eaedea78c814167f97565068554dc46b9e584
osushichannel <osushi_git@yahoo.co.jp>
2018-10-20 15:57:38 +0900

    main.pyにNLTKを組み込む main.htmlのcssのbackground-colorをfuchsiaに変更

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import eel
 import requests
 from bs4 import BeautifulSoup
+from nltk.tokenize import sent_tokenize
 
 eel.init("web")
 
 @eel.expose
 def add_popup(input_words):
-    input_words = input_words.split()
-    input_words = [word.rstrip("\.") for word in input_words]
+    sentences = sent_tokenize(input_words)
 
     output_words = ""
-    for en in input_words:
+    for en in sentences:
         url = "https://script.google.com/macros/s/AKfycbyMd"\
               "s6HiWhtHjnK0FiAYlhcKSa4sns6UIt-iZH1jS9-rCbyK"\
               "p07/exec?text=" + en + "&source=en&target=ja"
 
         ja = requests.get(url).text
         output_words += f'<span title="{ja}">{en}</span> '
-
-    output_words = output_words.rstrip()
-    output_words += "."
 
     eel.show(output_words)
 
